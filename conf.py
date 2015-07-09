b84544fa0b1aec54c74180198a18a76b8f36ab1c
g. nicholas d'andrea <gnidan@dramafever.com>
2015-07-09 14:46:11 -0400

    Make latex build the requirements PDF as its own document

--- conf.py
+++ conf.py
@@ -44,6 +44,7 @@ source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
+requirements_doc = 'requirements/index'
 
 # General information about the project.
 project = u'foodtastechess'
@@ -228,8 +229,10 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'foodtastechess.tex', u'foodtastechess Documentation',
-   u'team food taste', 'manual'),
+    (requirements_doc, 'requirements.tex', u'foodtastechess Requirements',
+     u'team food taste', 'manual'),
+#  (master_doc, 'foodtastechess.tex', u'foodtastechess Documentation',
+#   u'team food taste', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
